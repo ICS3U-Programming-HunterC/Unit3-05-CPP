9d85fe0576cb3611d6de12104871cf90ef6c90f4
Ubuntu <ubuntu@ip-172-31-80-220.ec2.internal>
2022-04-03 18:29:49 +0000

    added month.cpp

---
+++ month.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2022 Hunter Connolly All rights reserved
+// Created by: Hunter Connolly
+// Created on: April 2, 2022
+// This program asks the user for the monsth as a number
+// between 1 and 12. It then display the month as a string
+//  to the user
+#include <iostream>
+
+
+int main() {
+    // declare variables
+    int userMonth;
+
+    // ask the user for the month
+    std::cout << "Enter a number for a month (i.e. 2 for February): ";
+    std::cin >> userMonth;
+
+    // state the month selected as a string
+    switch (userMonth) {
+        case 1 :
+        std::cout << userMonth << " represents January. \n";
+        break;
+
+        case 2 :
+        std::cout << userMonth << " represents February. \n";
+        break;
+
+        case 3 :
+        std::cout << userMonth << " represents March. \n";
+        break;
+
+        case 4 :
+        std::cout << userMonth << " represents April. \n";
+        break;
+
+        case 5 :
+        std::cout << userMonth << " represents May. \n";
+        break;
+
+        case 6 :
+        std::cout << userMonth << " represents June. \n";
+        break;
+
+        case 7 :
+        std::cout << userMonth << " represents July. \n";
+        break;
+
+        case 8 :
+        std::cout << userMonth << " represents August. \n";
+        break;
+
+        case 9 :
+        std::cout << userMonth << " represents September. \n";
+        break;
+
+        case 10 :
+        std::cout << userMonth << " represents October. \n";
+        break;
+
+        case 11 :
+        std::cout << userMonth << " represents November. \n";
+        break;
+
+        case 12 :
+        std::cout << userMonth << " represents December. \n";
+        break;
+
+        // handle the error case
+        default :
+        std::cout << "ERROR! " << userMonth << " doesn't represent a month. \n";
+    }
+}
